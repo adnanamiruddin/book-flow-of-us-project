41b345ae412c2678500de4afe42d5eb49b83b278
trismantegar48 <trisman4848@gmail.com>
2023-05-26 21:31:12 +0800

    feat: Create parent class for models named Model

--- app/src/main/java/com/developersoffxinnovate/bookflowofus/models/Mahasiswa.java
+++ app/src/main/java/com/developersoffxinnovate/bookflowofus/models/Mahasiswa.java
@@ -1,57 +1,53 @@
 package com.developersoffxinnovate.bookflowofus.models;
 
-public class Mahasiswa {
-    private int id;
+public class Mahasiswa extends Model {
     private String nama;
     private String nim;
     private String prodi;
     private String alamat;
     private String noTelp;
     private String password;
-    
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+
     public String getNama() {
         return nama;
-    }
+    }    
     public void setNama(String nama) {
         this.nama = nama;
     }
+
     public String getNim() {
         return nim;
     }
     public void setNim(String nim) {
         this.nim = nim;
     }
+
     public String getProdi() {
         return prodi;
     }
     public void setProdi(String prodi) {
         this.prodi = prodi;
     }
+
     public String getAlamat() {
         return alamat;
     }
     public void setAlamat(String alamat) {
         this.alamat = alamat;
     }
+
     public String getNoTelp() {
         return noTelp;
     }
     public void setNoTelp(String noTelp) {
         this.noTelp = noTelp;
     }
+
     public String getPassword() {
         return password;
     }
     public void setPassword(String password) {
         this.password = password;
     }
-
-
 
 }
---
+++ app/src/main/java/com/developersoffxinnovate/bookflowofus/models/Model.java
@@ -0,0 +1,13 @@
+package com.developersoffxinnovate.bookflowofus.models;
+
+public class Model {
+    protected int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
