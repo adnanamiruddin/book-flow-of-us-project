dc8b62b84de9d1f4efe904112db6d26d611897d9
trismantegar48 <trisman4848@gmail.com>
2023-05-26 21:38:19 +0800

    fix: Extend class Model to class DataPeminjamanBuku

--- app/src/main/java/com/developersoffxinnovate/bookflowofus/models/DataPeminjamanBuku.java
+++ app/src/main/java/com/developersoffxinnovate/bookflowofus/models/DataPeminjamanBuku.java
@@ -1,43 +1,40 @@
 package com.developersoffxinnovate.bookflowofus.models;
 
-public class DataPeminjamanBuku {
-    private int id;
+public class DataPeminjamanBuku extends Model {
     private int idMahasiswa;
     private int idBuku;
     private String tanggalPinjam;
     private String tanggalKembali;
     private String status;
-    
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+
     public int getIdMahasiswa() {
         return idMahasiswa;
     }
     public void setIdMahasiswa(int idMahasiswa) {
         this.idMahasiswa = idMahasiswa;
     }
+
     public int getIdBuku() {
         return idBuku;
     }
     public void setIdBuku(int idBuku) {
         this.idBuku = idBuku;
     }
+
     public String getTanggalPinjam() {
         return tanggalPinjam;
     }
     public void setTanggalPinjam(String tanggalPinjam) {
         this.tanggalPinjam = tanggalPinjam;
     }
+
     public String getTanggalKembali() {
         return tanggalKembali;
     }
     public void setTanggalKembali(String tanggalKembali) {
         this.tanggalKembali = tanggalKembali;
     }
+    
     public String getStatus() {
         return status;
     }
