e9e7f3f2e2e6bd6a60b8a2df96342056651367ad
trismantegar48 <trisman4848@gmail.com>
2023-05-26 21:34:10 +0800

    fix: Extend class Model to class Book

--- app/src/main/java/com/developersoffxinnovate/bookflowofus/models/Book.java
+++ app/src/main/java/com/developersoffxinnovate/bookflowofus/models/Book.java
@@ -1,43 +1,40 @@
 package com.developersoffxinnovate.bookflowofus.models;
 
-public class Book {
-    private int id;
+public class Book extends Model {
     private String judul;
     private String pengarang;
     private String penerbit;
     private int tahunterbit;
     private int stok;
-    
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
+
     public String getJudul() {
         return judul;
     }
     public void setJudul(String judul) {
         this.judul = judul;
     }
+
     public String getPengarang() {
         return pengarang;
     }
     public void setPengarang(String pengarang) {
         this.pengarang = pengarang;
     }
+
     public String getPenerbit() {
         return penerbit;
     }
     public void setPenerbit(String penerbit) {
         this.penerbit = penerbit;
     }
+
     public int getTahunterbit() {
         return tahunterbit;
     }
     public void setTahunterbit(int tahunterbit) {
         this.tahunterbit = tahunterbit;
     }
+    
     public int getStok() {
         return stok;
     }
