f3e5251a44a95e8e303c56a8f78af2b1690dd89b
andisepul <sepulaksa445@gmail.com>
2023-05-26 23:35:40 +0800

    feat: Add new constructor for class Admin

--- app/src/main/java/com/developersoffxinnovate/bookflowofus/models/Admin.java
+++ app/src/main/java/com/developersoffxinnovate/bookflowofus/models/Admin.java
@@ -1,8 +1,14 @@
 package com.developersoffxinnovate.bookflowofus.models;
 
-public class Admin {
+public class Admin extends Model {
     private String user;
     private String password;
+
+    public Admin(int id, String user, String password) {
+        super(id);
+        this.user = user;
+        this.password = password;
+    }
 
     public String getUser() {
         return user;
