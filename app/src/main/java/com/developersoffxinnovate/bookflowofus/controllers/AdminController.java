092e0118891b0063128207f3993c67e0cd4bb7cc
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-05-28 14:38:18 +0800

    feat: Create new method getAdminByUser on AdminController

--- app/src/main/java/com/developersoffxinnovate/bookflowofus/controllers/AdminController.java
+++ app/src/main/java/com/developersoffxinnovate/bookflowofus/controllers/AdminController.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.developersoffxinnovate.bookflowofus.config.DatabaseConfig;
+import com.developersoffxinnovate.bookflowofus.models.Admin;
 
 public class AdminController extends DatabaseConfig {
 
@@ -22,5 +23,29 @@ public class AdminController extends DatabaseConfig {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public static Admin getAdminByUser(String user) {
+        Admin admin = null;
+        connection();
+        query = "SELECT * FROM admin WHERE user=?";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, user);
+
+            try (ResultSet adminResult = preparedStatement.executeQuery()) {
+                while (adminResult.next()) {
+                    int idAdmin = adminResult.getInt("id");
+                    String userAdmin = adminResult.getString("user");
+                    String passwordAdmin = adminResult.getString("password");
+
+                    admin = new Admin(idAdmin, userAdmin, passwordAdmin);
+                    return admin;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return admin;
     }
 }
